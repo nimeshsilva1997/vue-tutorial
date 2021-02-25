4c1d5fe83d17c279094d730bb9326173e47445d9
Nimesh Ryan Silva <nimeshsilva@gmail.com>
2021-02-25 16:11:38 -0500

    added files

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/** @format */
+
+const app = new Vue({
+  el: "#app",
+  data: {
+    hello: "Hello World",
+  },
+});
+const app2 = new Vue({
+  el: "#app2",
+  data: {
+    hello: "Hello World!",
+  },
+});
